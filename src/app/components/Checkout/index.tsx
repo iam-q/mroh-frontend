7a1ef4c56d7b701e59118d525c1e5ffc36b1f70d
Mistah Oh <iam.qsoh@gmail.com>
2026-01-08 19:24:23 -0800

    update settings

--- src/app/components/Checkout/index.tsx
+++ src/app/components/Checkout/index.tsx
@@ -9,14 +9,17 @@ import { loadStripe } from '@stripe/stripe-js'
 import { fetchClientSecret } from '../../utils/stripe';
 
 const stripePublishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-
-if (!stripePublishableKey) {
-  throw new Error('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not set')
-}
-
-const stripePromise = loadStripe(stripePublishableKey)
+const stripePromise = stripePublishableKey ? loadStripe(stripePublishableKey) : null
 
 export default function Checkout() {
+  if (!stripePromise) {
+    return (
+      <div id="checkout">
+        Payments are currently unavailable.
+      </div>
+    )
+  }
+
   return (
     <div id="checkout">
       <EmbeddedCheckoutProvider
